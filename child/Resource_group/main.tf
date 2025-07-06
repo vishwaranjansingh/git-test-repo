0694600774ead6e74c49d37e688d048b7ceebe88
Vaidehi-AWS <viswaranjan55@gmail.com>
2025-07-07 00:05:09 +0530

    test

--- child/Resource_group/main.tf
+++ child/Resource_group/main.tf
@@ -4,11 +4,6 @@ resource "azurerm_resource_group" "rgs" {
     location = each.value.location
   
 }
-resource "azurerm_resource_group" "rgs" {
-    for_each = var.rg
-    name = each.value.name
-    location = each.value.location
-  
-}
+
 
 
