66cf3caa21f31f58b9564f57dd54793b93af7693
Vaidehi-AWS <viswaranjan55@gmail.com>
2025-07-06 23:49:41 +0530

    resource add

--- child/Resource_group/main.tf
+++ child/Resource_group/main.tf
@@ -2,13 +2,9 @@ resource "azurerm_resource_group" "rgs" {
     for_each = var.rg
     name = each.value.name
     location = each.value.location
-  
 }
 resource "azurerm_resource_group" "rgs" {
     for_each = var.rg
     name = each.value.name
     location = each.value.location
-  
 }
-
-
